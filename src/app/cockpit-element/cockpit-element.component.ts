269a932a3b689aa8f4a6ed8ea2af5bece766b1a1
Helmut Guggenbichler <helmut.guggenbichler@skidata.com>
2021-06-30 11:48:14 +0200

    new

--- src/app/cockpit-element/cockpit-element.component.ts
+++ src/app/cockpit-element/cockpit-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit-element.component.scss']
 })
 export class CockpitElementComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent:string}>();
+  @Output()blueprintCreated = new EventEmitter<{serverName: string, serverContent:string}>();
   newServerName = '';
   newServerContent = '';
   serverElements = [];
+ 
 
   constructor() { }
 
@@ -16,19 +19,17 @@ export class CockpitElementComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-     content: this.newServerContent
-    });
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
   }
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'server-element',
+  selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.scss']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: { type: string, name: string, content: string }; // define a type
+  @Input('srvElement') element: { type: string, name: string, content: string }; // define a type
 
   constructor() { }
 
